009f1a6dbd6aed1381804de87d9b3d03554a0311
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-04-24 08:53:42 -0700

    Task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-$(document).ready(function() {
-  $.get("https://stefanbohacek.com/hellosalut/?lang=fr", function(data) {
-    $("#hello").text(data.hello);
+$(document).ready(function () {
+  let amenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    lista = Object.values(amenities);
+    if (lista.length > 0) {
+      $('.amenities > h4').text(lista.join(', '));
+    } else {
+      $('.amenities > h4').html('&nbsp;')
+    }
   });
 });
